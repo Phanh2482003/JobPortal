72939942dfc294928cf32c20eb81fcddff454e13
Phanh2482003 <155126167+Phanh2482003@users.noreply.github.com>
2024-12-16 01:31:36 +0700

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,55 @@
+<?php return array (
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'tonysm/globalid-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Tonysm\\GlobalId\\GlobalIdServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'GlobalId' => 'Tonysm\\GlobalId\\GlobalIdFacade',
+    ),
+  ),
+  'tonysm/rich-text-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Tonysm\\RichTextLaravel\\RichTextLaravelServiceProvider',
+    ),
+  ),
+);
